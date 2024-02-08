00560bb5a82bc216c3b06b41c0ea01fc09858a5d
giosciafra <francescogioscia8@gmail.com>
2024-02-08 17:36:57 +0100

    generati numeri casuali e fatte funzioni per le bombe

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
-// Consegna
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-// Potete realizzare lo stile che volete, sin quando tutto è leggibile con facilità 
-
-// Bonus
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-
-// Consigli del giorno:  
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-
+// Il computer deve generare 16 numeri casuali e inserirli in un array, in base al range della difficoltà prescelta (se abbiamo scelto facile l'array conterrà numeri casuali da 1 a 100,
+// se invece abbiamo scelto difficile l'array dovrà contenerne da 1 a 49): questi rappreseranno le posizioni delle nostre bombe :bomba:.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. 
+//Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+// BONUS 1
+// Quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle.
+// BONUS 2
+// Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
 
 
 
 const gridElement = document.querySelector('#grid');
-const playButton = document.querySelector('#play'); 
+const playButton = document.querySelector('#play');
 const levelElement = document.querySelector('#level');
 
 
@@ -29,14 +24,14 @@ let changeLevel = levelElement.value;
 console.log(changeLevel);
 
 levelElement.addEventListener('change',
-function() {
+    function () {
 
-    changeLevel = levelElement.value;
-    console.log('livello', changeLevel);
-}
+        changeLevel = levelElement.value;
+        console.log('livello', changeLevel);
+    }
 )
 
-playButton.addEventListener('click', function() {
+playButton.addEventListener('click', function () {
 
 
     // rimozione griglie (partite precedenti) con 'remove'
@@ -50,6 +45,8 @@ playButton.addEventListener('click', function() {
         gridElement.removeChild(gridElement.firstChild);
     }
 
+    //  generazione array di bombe
+    const bombPositions = generateBombPositions(changeLevel);
 
     // console.log("play");
 
@@ -59,12 +56,14 @@ playButton.addEventListener('click', function() {
         gridLevel = 81;
     } else if (changeLevel === "hard") {
         gridLevel = 49;
-    } else  {
-    gridLevel = 100;
+    } else {
+        gridLevel = 100;
     }
 
-// creazione griglia
+
+    // creazione griglia
     for (let i = 0; i < gridLevel; i++) {
+
 
         if (changeLevel == "medium") {
             gridElement.classList.add("medium");
@@ -73,7 +72,6 @@ playButton.addEventListener('click', function() {
         if (changeLevel == "hard") {
             gridElement.classList.add("hard");
         }
-
 
         //creo la box della griglia
         const newBox = document.createElement('div');
@@ -88,21 +86,62 @@ playButton.addEventListener('click', function() {
             newBox.classList.add('hard');
         }
 
+        newBox.innerHTML = [i + 1];
 
-        newBox.innerHTML = [i+1];
-    
 
-    newBox.addEventListener('click', function() {
+        newBox.addEventListener('click', function () {
 
-        console.log(this);
+            // console.log(this);
+            newBox.classList.add('active');
 
-        
-        newBox.classList.toggle('active');
-        
+
+            // gestiamo il click sulla bomba
+            const boxIndex = parseInt(this.innerHTML);
+
+            if (bombPositions.includes(boxIndex)) {
+
+                alert('Hai cliccato su una bomba! Game Over.');
+
+                resetGame();
+            }
+
+        });
+
+        gridElement.append(newBox);
     }
-    )
+});
 
-    gridElement.append(newBox);
+
+// funzione per generare array di bombe
+function generateBombPositions(changeLevel) {
+    const bombPositions = [];
+
+    let maxNumber;
+
+    if (changeLevel === 'hard') {
+        maxNumber = 49;
+
+    } if  (changeLevel === 'medium') {
+            maxNumber = 81;
+    } if (changeLevel === 'easy') {
+        maxNumber = 100;
+    }
+
+    while (bombPositions.length < 16) {
+        const randomPosition = Math.floor(Math.random() * maxNumber) + 1;
+
+        // if affinchè non ci siano duplicati
+        if (!bombPositions.includes(randomPosition)) {
+            bombPositions.push(randomPosition);
+        }
+    }
+    console.log('Posizioni delle bombe:', bombPositions);
+
+    return bombPositions;
 }
+
+function resetGame() {
+
 }
-)
+
+
